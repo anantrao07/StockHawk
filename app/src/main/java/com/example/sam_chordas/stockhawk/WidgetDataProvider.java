f6bb053121cc75ddde858c1692f441550a928eee
anant <anant.rao07@gmail.com>
2016-07-21 00:50:34 -0300

    changing table adding time stamp to database

--- app/src/main/java/com/example/sam_chordas/stockhawk/WidgetDataProvider.java
+++ app/src/main/java/com/example/sam_chordas/stockhawk/WidgetDataProvider.java
@@ -139,7 +139,7 @@ public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory
      */
     @Override
     public int getViewTypeCount() {
-        return 0;
+        return 1;
     }
 
     /**
